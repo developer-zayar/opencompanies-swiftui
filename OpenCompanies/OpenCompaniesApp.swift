e457c87cf95dd59bc2e9b32e144e2006effced8b
ZYP <zayarphyo16.mm@gmail.com>
2025-09-27 13:46:04 +0800

    Initial Commit

---
+++ OpenCompanies/OpenCompaniesApp.swift
@@ -0,0 +1,17 @@
+//
+//  OpenCompaniesApp.swift
+//  OpenCompanies
+//
+//  Created by Zay Yar Phyo on 27/09/2025.
+//
+
+import SwiftUI
+
+@main
+struct OpenCompaniesApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
